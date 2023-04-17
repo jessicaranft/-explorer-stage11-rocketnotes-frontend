e95c65742efb09ce81c7f5690445766339164161
Jessica Ranft <jessica.ranft@gmail.com>
2023-04-17 11:07:34 -0300

    first commit

---
+++ src/components/Button/styles.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Container = styled.button`
+  width: 100%;
+  height: 56px;
+
+  background-color: ${({ theme }) => theme.COLORS.ORANGE};
+  color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
+
+  border: 0;
+  border-radius: 10px;
+  padding: 0 16px;
+  margin-top: 16px;
+
+  font-weight: 500;
+
+  &:disabled {
+    opacity: 0.5;
+  }
+`;
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,35 @@
+import { RiShutDownLine } from 'react-icons/ri';
+import { useNavigate } from 'react-router-dom';
+
+import { useAuth } from '../../hooks/auth';
+import { Container, Profile, Logout } from "./styles";
+import { api } from '../../services/api';
+import avatarPlaceholder from '../../assets/avatar_placeholder.svg';
+
+export function Header() {
+  const { signOut, user } = useAuth();
+  const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder;
+  const navigate = useNavigate();
+
+  function handleSignOut() {
+    navigate("/");
+    signOut();
+  }
+
+  return (
+    <Container>
+      <Profile to="/profile">
+        <img src={avatarUrl} alt={`Foto do usuário ${user.name}`} />
+
+        <div>
+          <span>Bem-vinda</span>
+          <strong>{user.name}</strong>
+        </div>
+      </Profile>
+
+      <Logout onClick={handleSignOut}>
+        <RiShutDownLine />
+      </Logout>
+    </Container>
+  );
+}
